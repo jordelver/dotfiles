7e9b970a364190ed6342db86a91fdf663b31eeef
Jordan Elver <jordan.elver@gmail.com>
2013-07-26 15:48:17 +0100

    Case insensitive completions for zsh

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -8,3 +8,6 @@ fpath=(
 
 source ~/.zsh/completions/git-flow-completion.zsh
 
+# Case-insensitive (all), partial-word and then substring completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
