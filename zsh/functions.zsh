51d588756d5f7b99cf7c8d0da87ab98bc0b5785f
Jordan Elver <jordan.elver@gmail.com>
2013-07-27 07:10:25 +0100

    Add some cool zsh functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -4,6 +4,34 @@ function rrg {
   rake routes | grep $1
 }
 
+function ex {
+  if [[ -f $1 ]]; then
+    case $1 in
+      *.tar.bz2) tar xvjf $1;;
+      *.tar.gz) tar xvzf $1;;
+      *.tar.xz) tar xvJf $1;;
+      *.tar.lzma) tar --lzma xvf $1;;
+      *.bz2) bunzip $1;;
+      *.rar) unrar $1;;
+      *.gz) gunzip $1;;
+      *.tar) tar xvf $1;;
+      *.tbz2) tar xvjf $1;;
+      *.tgz) tar xvzf $1;;
+      *.zip) unzip $1;;
+      *.Z) uncompress $1;;
+      *.7z) 7z x $1;;
+      *.dmg) hdiutul mount $1;; # mount OS X disk images
+      *) echo "'$1' cannot be extracted via >ex<";;
+  esac
+  else
+      echo "'$1' is not a valid file"
+  fi
+}
+
+function last_modified() { # by Ryan Bates
+  ls -t $* 2> /dev/null | head -n 1
+}
+
 # Use rbenv if available
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
