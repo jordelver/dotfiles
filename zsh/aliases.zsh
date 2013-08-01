978515c850df689f5acc4685c954286ce880043f
Jordan Elver <jordan.elver@gmail.com>
2013-08-01 15:22:45 +0100

    Use pretty formatting by default for cucumber

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -38,7 +38,7 @@ alias tunnel='ssh -D 8080 -f -N -C m3lky' # Setup a reverse SSH tunnel
 # Rails / Ruby
 alias b='bundle'
 alias be='bundle exec'
-alias bec='bundle exec cucumber'
+alias bec='bundle exec cucumber --format=pretty'
 alias sc='script/console'
 alias ss='script/server'
 
