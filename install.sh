cf96749f466714c49bac74aa4aca7cd6659fb9f9
Jordan Elver <jordan.elver@gmail.com>
2018-08-11 21:48:57 +0100

    Install from Brewfile as well as link configs

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+
+# Install everything from Brewfile
+brew bundle
+
+# Link dotfiles into place
 echo "Linking config files into place"
 for dotfile in *; do
   source="$PWD/$dotfile"
   target="$HOME/.$dotfile"
-  if [[ "$dotfile" != "README.md" && "$dotfile" != "install.sh" ]]; then
+  if [[ "$dotfile" != "README.md" && "$dotfile" != "install.sh" && "$dotfile" != "Brewfile" ]]; then
     if [ ! -L "$target" ]; then
       echo "* $source -> $target"
       ln -s "$source" "$target"
