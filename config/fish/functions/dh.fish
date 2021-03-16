1c40eafb5d829b8b9251ca572e9740f002fefa1c
Jordan Elver <jordan@elver.me>
2021-03-16 16:14:20 +0000

    Change `dh` to support multi entries at once
    Additionally, we don't ask before deleting entries now. Previously we
    would select a history entry and then be shunted into the history's
    interactive prompt to finish the job.
    
    We don't bother with that any more because it was a barrier to use.

--- config/fish/functions/dh.fish
+++ config/fish/functions/dh.fish
@@ -1,3 +1,5 @@
 function dh -d "Fuzzily delete entries from your history"
-  history | fzf | read -l item; and history delete --prefix "$item"
+  history | fzf --multi | while read -l item
+    history delete --case-sensitive --exact "$item"
+  end
 end
