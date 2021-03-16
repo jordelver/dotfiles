7aac7ac846b00c39e367d7864f52cc43e498de23
Jordan Elver <jordan@elver.me>
2021-03-16 16:17:14 +0000

    Fix `pr` helper for creating PRs easily

--- config/fish/functions/pr.fish
+++ config/fish/functions/pr.fish
@@ -1,3 +1,3 @@
-function v -d "Create a draft PR on GitHub for the current branch"
+function pr -d "Create a draft PR on GitHub for the current branch"
   gh pr create --assignee jordelver --draft
 end
