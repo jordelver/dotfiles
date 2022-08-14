25f2a0a8beb0ff89354c1d6e57428d16a3531dd8
Jordan Elver <jordan@elver.me>
2022-08-14 22:11:33 +0100

    Remove unused command aliases
    I'm pretty sure I don't use these anyway, so removing for now. I can
    always bring them back if I'm mistaken.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,16 +1,5 @@
 require("options")
 require("keymaps")
-
--- Alias some keys
-vim.cmd [[
-  " Alias :Q to :q
-  command! Q q
-  command! Qall qall
-
-  " Alias :Wq to :wq
-  command! Wq wq
-]]
-
 require("plugins")
 require("colors")
 require("functions")
