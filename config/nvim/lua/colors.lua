2f571c9b8ddb36c8636b0db8549f4b65a208bb8d
Jordan Elver <jordan@elver.me>
2022-08-14 23:06:47 +0100

    Convert colorscheme overrides into Lua

--- config/nvim/lua/colors.lua
+++ config/nvim/lua/colors.lua
@@ -1,33 +1,36 @@
+-- Taken from https://github.com/goolord/nvim/blob/main/lua/modules/colors.lua
+
+local function hl(group, options)
+  vim.api.nvim_set_hl(0, group, options)
+end
+
 -- Highlight commands to override the main colorscheme
-vim.cmd [[
-  " Override colorscheme with my own highlights every
-  " time a colorscheme is sourced
-  augroup OverrideColorScheme
-    autocmd!
-    autocmd ColorScheme * call ColorSchemeOverrides()
-  augroup END
+local function apply_colors()
 
-  function! ColorSchemeOverrides() abort
-    " Override `CursorLine` highlighting
-    highlight CursorLine        guifg=NONE guibg=#191919
-    highlight CursorColumn      guifg=NONE guibg=#191919
+  -- Override `CursorLine` highlighting
+  hl("CursorLine", { fg = nil, bg = "#191919" })
+  hl("CursorColumn", { fg = nil, bg = "#191919" })
 
-    " Override search highlighting
-    highlight Search            guifg=#080808 guibg=#ffd700 gui=NONE
-    highlight Searchlight       guifg=#080808 guibg=#ffaf00 gui=NONE
+  -- Override search highlighting
+  hl("Search", { fg = "#080808", bg = "#ffd700" })
+  hl("Searchlight", { fg = "#080808", bg = "#ffaf00" })
 
-    " Highlight text that goes over 50 characters in git commit messages
-    highlight gitcommitOverflow guifg=#cc0000 guibg=NONE
+  -- Highlight text that goes over 50 characters in git commit messages
+  hl("gitcommitOverflow", { fg = "#cc0000", bg = nil })
 
-    " Highlight TODO comments
-    highlight rubyTODO          guifg=#eeeeee guibg=#2d2c5d
-    highlight elixirTodo        guifg=#eeeeee guibg=#2d2c5d
-    highlight vimTODO           guifg=#eeeeee guibg=#2d2c5d
-    highlight jsCommentTodo     guifg=#eeeeee guibg=#2d2c5d
-    highlight rustTodo          guifg=#eeeeee guibg=#2d2c5d
-    highlight luaTodo           guifg=#eeeeee guibg=#2d2c5d
-  endfunction
-]]
+  -- Highlight TODO comments
+  hl("rubyTODO",      { fg = "#eeeeee", bg = "#2d2c5d" })
+  hl("elixirTodo",    { fg = "#eeeeee", bg = "#2d2c5d" })
+  hl("vimTODO",       { fg = "#eeeeee", bg = "#2d2c5d" })
+  hl("jsCommentTodo", { fg = "#eeeeee", bg = "#2d2c5d" })
+  hl("rustTodo",      { fg = "#eeeeee", bg = "#2d2c5d" })
+  hl("luaTodo",       { fg = "#eeeeee", bg = "#2d2c5d" })
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = apply_colors,
+})
 
 -- Set colorscheme (theme)
 vim.cmd [[ colorscheme moonlight ]]
