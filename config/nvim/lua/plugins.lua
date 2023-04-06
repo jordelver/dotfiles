ab92bd5cc57600c286766f8f346e9dc342a3787a
Jordan Elver <jordan@elver.me>
2023-04-06 23:26:28 +0100

    Only use regex matching when highlighting words
    I'm only really looking to match word tokens to make sure they match or
    are spelt correctly and the LSP tries to do too much and doesn't seem to
    do a great job.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -279,6 +279,12 @@ packer.startup(function(use)
     "RRethy/vim-illuminate",
     config = function()
       require('illuminate').configure({
+        providers = {
+          -- Strangely, we only use regex matching. I'm only really looking to
+          -- match word tokens to make sure they match or are spelt correctly
+          -- and the LSP tries to do too much and doesn't seem to do a great job.
+          'regex',
+        },
         filetypes_denylist = {
           "markdown",
         }
