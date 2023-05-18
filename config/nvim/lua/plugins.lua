6c959567cbc3d31f99f9ea8de4499a47c35be859
Jordan Elver <jordan@elver.me>
2023-05-18 13:39:16 +0100

    Use `nvim-colorizer` to colorize "colour" strings
    This works with Tailwind colours too, and is apparently faster...
    
    For Elixir files with markup in them we change the display of colours to
    colour the foreground (the actual text) instead of the background. This
    is primarily for the benefit of Tailwind classes in `class` attributes
    where the coloured backgrounds are very jarring.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -272,7 +272,32 @@ packer.startup(function(use)
   use { "scrooloose/nerdtree" }
 
   -- Colorize hex css-like colours in code
-  use { "ap/vim-css-color" }
+  use({
+    "NvChad/nvim-colorizer.lua",
+    config = function()
+      require ("colorizer").setup({
+        filetypes = {
+          'css',
+          'scss',
+          'javascript',
+          eelixir = { mode = "foreground" }
+        },
+        user_default_options = {
+          RGB = true,          -- #RGB hex codes
+          RRGGBB = true,       -- #RRGGBB hex codes
+          names = true,        -- "Name" codes like Blue or blue
+          RRGGBBAA = true,     -- #RRGGBBAA hex codes
+          AARRGGBB = true,     -- 0xAARRGGBB hex codes
+          rgb_fn = true,       -- CSS rgb() and rgba() functions
+          hsl_fn = true,       -- CSS hsl() and hsla() functions
+          mode = "background", -- Set the display mode.
+          tailwind = true,     -- Enable tailwind colors
+          always_update = true -- Update color values even if buffer is not focused
+                               -- For example: cmp_menu, cmp_docs
+        },
+      })
+    end
+  })
 
   -- Highlight word under cursor on hover
   use {
