bb550a449cd9bb2c33b979b3c1bf63f1c823e15e
Jordan Elver <jordan@elver.me>
2023-09-04 15:44:20 +0100

    Allow opening multiple files from Telescope results
    If multiple file selections are detected, open each file. If hitting
    <CR> on a single selection fall back to the default behaviour.
    
    From: https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1679797700

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -200,8 +200,34 @@ packer.startup(function(use)
       {"nvim-lua/plenary.nvim"}
     },
     config = function ()
+
+      -- If multiple file selections are detected, open each file. If hitting
+      -- <CR> on a single selection fall back to the default behaviour.
+      -- https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1679797700
+      local select_one_or_multi = function(prompt_bufnr)
+        local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+        local multi = picker:get_multi_selection()
+        if not vim.tbl_isempty(multi) then
+          require('telescope.actions').close(prompt_bufnr)
+          for _, j in pairs(multi) do
+            if j.path ~= nil then
+              vim.cmd(string.format("%s %s", "edit", j.path))
+            end
+          end
+        else
+          require('telescope.actions').select_default(prompt_bufnr)
+        end
+      end
+
       require("telescope").setup({
         defaults = {
+          mappings = {
+            i = {
+              -- Open files on <Enter>. See above.
+              ["<CR>"] = select_one_or_multi,
+            }
+          },
+
           -- Until this is fixed: https://github.com/nvim-telescope/telescope.nvim/issues/2667
           sorting_strategy = 'ascending'
         }
