3f5722f0eefb175cc65c08921e2e569a79913539
Jordan Elver <jordan@elver.me>
2023-09-06 19:40:41 +0100

    Remove `hg` function as we have Treesitter now
    Instead, use `:Inspect`.

--- config/nvim/lua/functions.lua
+++ config/nvim/lua/functions.lua
@@ -1,14 +1,4 @@
 vim.cmd [[
-  " Show syntax highlighting groups for word under cursor
-  " Useful for changing theme colours
-  nmap <leader>hg :call SynStack()<CR>
-  function! SynStack()
-    if !exists("*synstack")
-      return
-    endif
-    echo map(synstack(line('.'), col('.')), 'synIDattr(v:val, "name")')
-  endfunc
-
   " Open a split for each dirty file in git
   function! OpenChangedFiles()
     only " Close all windows, unless they're modified
