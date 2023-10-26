c04f2e67fec7dc8addc56ba47ca4cfc2978533d2
Jordan Elver <jordan@elver.me>
2023-10-26 12:10:54 +0100

    Remove the `:BD` (buffer delete) command
    We now have a way to delete buffers from Telescope.

--- config/nvim/lua/functions.lua
+++ config/nvim/lua/functions.lua
@@ -31,14 +31,4 @@ vim.cmd [[
     redir END
     return split(list, "\n")
   endfunction
-
-  function! s:delete_buffers(lines)
-    execute 'bwipeout' join(map(a:lines, {_, line -> split(line)[0]}))
-  endfunction
-
-  command! BD call fzf#run(fzf#wrap({
-    \ 'source': s:list_buffers(),
-    \ 'sink*': { lines -> s:delete_buffers(lines) },
-    \ 'options': '--multi --reverse --bind ctrl-a:select-all+accept'
-    \ }))
 ]]
