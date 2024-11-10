1866fbbaa79e333f98e3ed93d91bc3ec3051306c
Jordan Elver <jordan@elver.me>
2024-11-10 11:40:41 +0000

    Add note about removing tpope/vim-unimpaired next release

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -86,6 +86,8 @@ packer.startup(function(use)
   -- Delete/change/add parentheses/quotes/XML-tags/much more with ease
   use { "tpope/vim-surround" }
 
+  -- TODO Remove this once the latest stable version is released
+  --      https://github.com/neovim/neovim/blob/f59aeb5a9ae80f6f0e12ae52330f3f4e0e7e03e0/runtime/doc/news.txt#L181
   -- Provides `[<Space>` and `]<Space>` for adding space above and below lines amongst others
   use { "tpope/vim-unimpaired" }
 
