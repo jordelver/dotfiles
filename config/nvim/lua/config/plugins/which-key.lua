9004bed63349c158babf708352275d3ff4a15c90
Jordan Elver <jordan@elver.me>
2025-05-27 11:54:38 +0100

    Enable `which-key` again and go back to vanilla config
    Something is not quite right, but not sure what so reverting to standard
    config options until we've used it for a bit.

--- config/nvim/lua/config/plugins/which-key.lua
+++ config/nvim/lua/config/plugins/which-key.lua
@@ -3,19 +3,6 @@ return {
   -- Useful for learning new keybindings
   {
     "folke/which-key.nvim",
-    enabled = false,
-    config = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 500 -- The amount of time to wait before showing which-key
-      require("which-key").setup {
-        window = {
-          border = "single", -- More space around the window
-          winblend = 5, -- Semi-transparent window
-        },
-        layout = {
-          align = "center" -- Align columns center
-        },
-      }
-    end
+    enabled = true
   }
 }
