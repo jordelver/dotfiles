3b0dd3ffe0a4ce4d4bf89c329343a4b2ec62302a
Jordan Elver <jordan@elver.me>
2025-05-28 11:02:08 +0100

    Alias Gbrowse -> GBrowse because I can't type

--- config/nvim/lua/config/plugins/vim-fugitive.lua
+++ config/nvim/lua/config/plugins/vim-fugitive.lua
@@ -1,7 +1,13 @@
 return {
   -- Git from Vim
-  -- Adds a _lot_ of commands for interacting with Git including `:Gbrowse`
-  { "tpope/vim-fugitive" },
+  -- Adds a _lot_ of commands for interacting with Git including `:GBrowse`
+  {
+    "tpope/vim-fugitive",
+    config = function()
+      -- Alias Gbrowse -> GBrowse because I can't type
+      vim.api.nvim_create_user_command('Gbrowse', 'GBrowse', {})
+    end
+  },
 
   -- Add GitHub support to Fugitive
   { "tpope/vim-rhubarb" }
