4077f16f00308b4cfb6e8a27b5cdd29d6453651c
Jordan Elver <jordan@elver.me>
2025-07-22 13:04:59 +0100

    Extract user commands to their own file
    I'm starting to add more user commands so having somewhere dedicated to
    them makes sense.

--- config/nvim/lua/config/plugins/vim-fugitive.lua
+++ config/nvim/lua/config/plugins/vim-fugitive.lua
@@ -1,13 +1,7 @@
 return {
   -- Git from Vim
   -- Adds a _lot_ of commands for interacting with Git including `:GBrowse`
-  {
-    "tpope/vim-fugitive",
-    config = function()
-      -- Alias Gbrowse -> GBrowse because I can't type
-      vim.api.nvim_create_user_command('Gbrowse', 'GBrowse', {})
-    end
-  },
+  { "tpope/vim-fugitive" },
 
   -- Add GitHub support to Fugitive
   { "tpope/vim-rhubarb" }
---
+++ config/nvim/lua/config/user_commands.lua
@@ -0,0 +1,2 @@
+-- Alias Gbrowse -> GBrowse because I can't type
+vim.api.nvim_create_user_command('Gbrowse', 'GBrowse', {})
