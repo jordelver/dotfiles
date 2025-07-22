1dad42bcb78f9d2e5c538e60f4aed611a3a1716d
Jordan Elver <jordan@elver.me>
2025-07-22 13:06:36 +0100

    Making :Q and :W case insensitive because I have fat fingers
    Like it or not, I cannot type

--- config/nvim/lua/config/user_commands.lua
+++ config/nvim/lua/config/user_commands.lua
@@ -1,2 +1,8 @@
+-- Making :Q and :W case insensitive because I have fat fingers
+-- https://www.reddit.com/r/neovim/comments/1lpvpgf/comment/n0y4x55/
+vim.api.nvim_create_user_command("Q", "q", { bang = true })
+vim.api.nvim_create_user_command("W", "w", { bang = true })
+vim.api.nvim_create_user_command("Wq", "wq", { bang = true })
+
 -- Alias Gbrowse -> GBrowse because I can't type
 vim.api.nvim_create_user_command('Gbrowse', 'GBrowse', {})
