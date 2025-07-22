a5ff6576892a0b71f87c181e03b5e2ca697c059d
Jordan Elver <jordan@elver.me>
2025-07-22 13:08:22 +0100

    Add fFtT-highlights.nvim plugin
    I'm hoping this will assist in using these motions more

---
+++ config/nvim/lua/config/plugins/ft-highlights.lua
@@ -0,0 +1,10 @@
+return {
+  "samiulsami/fFtT-highlights.nvim",
+  config = function()
+    ---@module "fFtT-highlights"
+    ---@type fFtT_highlights.opts
+    require("fFtT-highlights"):setup({
+      ---See below for default configuration options
+    })
+  end
+}
